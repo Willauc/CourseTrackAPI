95733870abb8e8f228da1b8cdd775a39115bd79f
William Turbide Auclair <william.turbide-auclair.1@ulaval.ca>
2026-01-20 11:54:00 -0500

    INIT CourseTrackAPI
    For codeRed website

---
+++ src/main/java/com/willauc/coursetrackapi/CourseTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.willauc.coursetrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseTrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseTrackApiApplication.class, args);
+    }
+
+}

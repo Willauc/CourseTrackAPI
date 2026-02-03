1f6d1a5b33ce07864a2cfaa9f4da7288c980d0f7
William Turbide Auclair <williamauclair22@gmail.com>
2026-02-02 19:14:05 -0500

    Entities folder created

---
+++ src/main/java/com/willauc/coursetrackapi/entities/Address.java
@@ -0,0 +1,7 @@
+package com.willauc.coursetrackapi.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Address {
+}
---
+++ src/main/java/com/willauc/coursetrackapi/entities/User.java
@@ -0,0 +1,8 @@
+package com.willauc.coursetrackapi.entities;
+
+import jakarta.persistence.Entity;
+
+
+@Entity
+public class User {
+}
